1cd0212a366370d69ce3a862b41907c5c5be5925
sammygallo <sammygallo@gmail.com>
2022-09-27 17:41:24 -0400

    Delete events-suggestor/backend directory

--- events-suggestor/backend/index.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config()
-const express = require('express')
-const app = express();
-
-app.listen(process.env.PORT, () => {
-    console.log(`Listening on ${process.env.PORT}`)
-})
