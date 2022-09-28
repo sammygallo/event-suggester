175d761f3d085258233e0a116a7d88f0d26a7b00
Priyanka Shah <priyanka2003shah@gmail.com>
2022-09-28 17:48:53 -0400

    direction, destintion added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ import {
   SkeletonText,
 } from '@chakra-ui/react'
 import { FaLocationArrow, FaTimes } from 'react-icons/fa'
-import { useJsApiLoader, GoogleMap, Marker, Autocomplete } from '@react-google-maps/api'
+import { useJsApiLoader, GoogleMap, Marker, Autocomplete, DirectionsRenderer, } from '@react-google-maps/api'
 import { useState, useRef } from 'react'
 //default center for google maps = eifel tower
 const center = { lat: 48.8584, lng: 2.2945 }
@@ -20,23 +20,45 @@ function App() {
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
     //libraries to be used
-    libraries:['places'],
+    libraries: ['places'],
   })
-  
-  const [map, setMap] = useState(/** @type google.maps.Map */ (null))
+
+  const [map, setMap] = useState(/** @type google.maps.Map */(null))
   const [directionsResponse, setDirectionsResponse] = useState(null)
   const [distance, setDistance] = useState('')
   const [duration, setDuration] = useState('')
-   /** @type React.MutableRefObject<HTMLInputElement> */
+  /** @type React.MutableRefObject<HTMLInputElement> */
   const originRef = useRef()
-   /** @type React.MutableRefObject<HTMLInputElement> */
+  /** @type React.MutableRefObject<HTMLInputElement> */
   const destiantionRef = useRef()
   //makes it look like its loading with skeletontext from chakra
   if (!isLoaded) {
     return <SkeletonText />
   }
-
-
+  async function calculateRoute() {
+    if (originRef.current.value === '' || destiantionRef.current.value === '') {
+      return
+    }
+    // eslint-disable-next-line no-undef
+    const directionsService = new google.maps.DirectionsService()
+    const results = await directionsService.route({
+      //value - input
+      origin: originRef.current.value,
+      destination: destiantionRef.current.value,
+      // eslint-disable-next-line no-undef
+      travelMode: google.maps.TravelMode.DRIVING,
+    })
+    setDirectionsResponse(results)
+    setDistance(results.routes[0].legs[0].distance.text)
+    setDuration(results.routes[0].legs[0].duration.text)
+  }
+  function clearRoute() {
+    setDirectionsResponse(null)
+    setDistance('')
+    setDuration('')
+    originRef.current.value = ''
+    destiantionRef.current.value = ''
+  }
   return (
     <Flex
       position='relative'
@@ -50,18 +72,20 @@ function App() {
       {/* google map box */}
       {/* doesnt load without map container style */}
       <GoogleMap center={center} zoom={15} mapContainerStyle={{ width: '100%', height: '100%' }}
-      options={{
-        zoomControl: false,
-        streetViewControl: false,
-        mapTypeControl: false,
-        fullscreenControl: false,
-      }}
-      onLoad={map => setMap(map)}
+        options={{
+          zoomControl: false,
+          streetViewControl: false,
+          mapTypeControl: false,
+          fullscreenControl: false,
+        }}
+        onLoad={map => setMap(map)}
       >
-        {/* zoom option */}
-        
+
         {/* display markers, directions */}
         <Marker position={center} />
+        {directionsResponse && (
+          <DirectionsRenderer directions={directionsResponse} />
+        )}
       </GoogleMap>
       <Box
         p={4}
@@ -73,36 +97,36 @@ function App() {
         zIndex='1'
       >
         <HStack spacing={4}>
-  {/*  autocomplete = automatic tries to complete origin location or destination when starting to type */}
+          {/*  autocomplete = automatic tries to complete origin location or destination when starting to type */}
           <Autocomplete>
-          <Input type='text' placeholder='Origin'  ref={originRef} />
+            <Input type='text' placeholder='Origin' ref={originRef} />
           </Autocomplete>
 
           <Autocomplete>
-          <Input type='text' placeholder='Destination' ref={destiantionRef}/>
+            <Input type='text' placeholder='Destination' ref={destiantionRef} />
           </Autocomplete>
 
           <ButtonGroup>
-            <Button colorScheme='pink' type='submit'>
+            <Button colorScheme='pink' type='submit' onClick={calculateRoute}>
               Calculate Route
             </Button>
             <IconButton
               aria-label='center back'
               icon={<FaTimes />}
-              onClick={() => alert(123)}
+              onClick={clearRoute}
             />
           </ButtonGroup>
         </HStack>
         <HStack spacing={4} mt={4} justifyContent='space-between'>
-          <Text>Distance: </Text>
-          <Text>Duration: </Text>
+          <Text>Distance: {distance} </Text>
+          <Text>Duration: {duration} </Text>
           <IconButton
             aria-label='center back'
             icon={<FaLocationArrow />}
             isRound
             onClick={() => {
               map.panTo(center)
-              
+
             }}
           />
         </HStack>
