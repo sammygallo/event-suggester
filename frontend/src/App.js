286c1506e45157fb05aea702a6f4dfde495667b3
Priyanka Shah <priyanka2003shah@gmail.com>
2022-09-28 13:34:33 -0400

    loads location box to add a google  map box

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Flex,
+  HStack,
+  IconButton,
+  Input,
+  Text,
+} from '@chakra-ui/react'
+import { FaLocationArrow, FaTimes } from 'react-icons/fa'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Flex
+      position='relative'
+      flexDirection='column'
+      alignItems='center' 
+      bgPos='bottom'
+      h='100vh'
+      w='100vw'
+    >
+      <Box position='absolute' left={0} top={0} h='100%' w='100%'></Box>
+{/* google map box */}
+
+      <Box
+        p={4}
+        borderRadius='lg'
+        mt={4}
+        bgColor='white'
+        shadow='base'
+        minW='container.md'
+        zIndex='modal'
+      >
+        <HStack spacing={4}>
+          <Input type='text' placeholder='Origin' />
+          <Input type='text' placeholder='Destination' />
+          <ButtonGroup>
+            <Button colorScheme='pink' type='submit'>
+              Calculate Route
+            </Button>
+            <IconButton
+              aria-label='center back'
+              icon={<FaTimes />}
+              onClick={() => alert(123)}
+            />
+          </ButtonGroup>
+        </HStack>
+        <HStack spacing={4} mt={4} justifyContent='space-between'>
+          <Text>Distance: </Text>
+          <Text>Duration: </Text>
+          <IconButton
+            aria-label='center back'
+            icon={<FaLocationArrow />}
+            isRound
+            onClick={() => alert(123)}
+          />
+        </HStack>
+      </Box>
+    </Flex>
+  )
 }
 
-export default App;
+export default App
