e0c3a370431898442cb95ca2195a8303a5d056e9
valgarciav <97136074+valgarciav@users.noreply.github.com>
2022-10-05 19:05:58 -0400

    map debug
    map not showing

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.js";
+<<<<<<< Updated upstream
 import { ChakraProvider, theme } from "@chakra-ui/react";
+=======
+//import { ThemeProvider } from "@mui/system";
+//import theme from "@mui/material/theme";
+>>>>>>> Stashed changes
 
 ReactDOM.render(
   <React.StrictMode>
